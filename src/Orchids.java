0b4cdf7725fce63b05db564c18bdce2059ff8b43
oujesky <oujesky@spsejecna.cz>
2024-03-11 10:44:19 +0100

    added new class Orchids

---
+++ src/Orchids.java
@@ -0,0 +1,5 @@
+public class Orchids extends Flower{
+    public Orchids(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+}
